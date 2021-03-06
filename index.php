b76e5574cce5704af25967534fb4ebe3f307e3b3
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-06 08:11:56 +0700

    oop php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+class Student
+{
+    
+}
+
+$classes = get_declared_classes();
+
+echo "Classes: " . implode(', ',  $classes) . "<br />";
+
+$classNames = ['Product', 'Student', 'student'];
+
+foreach ($classNames as $className) {
+    if (class_exists($className)) {
+        echo "{$className} class ada. <br />";
+    } else {
+        echo "{$className} class tidak ada. <br />";
+    }
+}
