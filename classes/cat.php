4ce92d422676a6246f3829dc70f3285234a5b837
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-09 20:59:54 +0700

    oop php

---
+++ classes/cat.php
@@ -0,0 +1,11 @@
+<?php
+
+class Cat
+{
+    public $name = 'Meow';
+
+    public function sayHello()
+    {
+        return "Hi, Meow";
+    }
+}
---
+++ classes/santri.php
@@ -0,0 +1,11 @@
+<?php
+
+class Santri
+{
+    public $name = 'Santri';
+
+    public function sayHello()
+    {
+        return "Hi, Santri";
+    }
+}
